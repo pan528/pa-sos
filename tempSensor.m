624df9f90631841da62474b0c938fb4d8a700170
pan528 <72293661+pan528@users.noreply.github.com>
2025-06-22 22:04:55 +0800

    Update tempSensor.m

--- tempSensor.m
+++ tempSensor.m
@@ -94,6 +94,7 @@ x_max = round(center_x + side_x/2);
 z_min = round(center_z - side_z/2);
 z_max = round(center_z + side_z/2);
 
+
 for i = 1:total_sim
     
     % scatters distributions 
@@ -109,6 +110,8 @@ for i = 1:total_sim
     c0_background = c0_random_seed; % sound speed [m/s]
     medium_sos = c0_background *ones(Nz, Nx); 
     medium_sos_echo = c0_background *ones(Nz, Nx);
+    % 背景密度：围绕 1020 kg/m³，局部有 ±3% 的稀疏扰动。
+    % 背景声速：每次仿真全局随机选取 1400~1600 m/s 的一个值，整个区域均匀分布。
     % 通过修改 medium_sos 和 medium_sos_echo 的值来实现不同区域的声速分布
      % inclusions 
      option=0; % 选择是否设置不同区域的声速分布
@@ -190,7 +193,7 @@ for i = 1:total_sim
         inclu_sos(kk) = sos_val;
         medium_sos(inclusions_region==1)= inclu_sos(kk);
         medium_sos_echo(inclusions_region==1) = inclu_sos(kk);
-
+        % 夹杂体内有 10% 的点会被赋予更高的声速用于回声增强（hyperechoic）
         % specify echongenicity (hyperechoic) in inclusions by assigning 10% grid points as the scatters 
         index_ellipse = find(inclusions_region==1); 
         hypo_index_ellipse = index_ellipse(randi(numel(index_ellipse),1,round(0.1*numel(index_ellipse))));
@@ -304,10 +307,32 @@ source.u_mask = sensor.mask;
 % 如果你关心源表面振动（如换能器阵元），建议用速度源。
 % 如果只需模拟点声源或已知的压力激励，可用压力源。
 
+sound_speed_center = medium.sound_speed(x_min:x_max, z_min:z_max);
+
+x_axis = ((x_min:x_max) - 1) * dx * 1e3; % 单位mm
+z_axis = ((z_min:z_max) - 1) * dx * 1e3; % 单位mm
+
+figure;
+imagesc(z_axis, x_axis, sound_speed_center);
+xlabel('z [mm]');
+ylabel('x [mm]');
+title('中心25mm×25mm范围内的声速分布');
+colorbar;
+axis equal tight;
+
 Nx1 = size(medium.density,1);
 Nz1 = size(medium.density,2);
 x = (0:Nx1-1)*dx*1e3; % 单位mm
 z = (0:Nz1-1)*dz*1e3; % 单位mm
+
+center_x_mm = (Nx/2-1)*dx*1e3; % 中心点x，单位mm
+center_z_mm = (Nz/2-1)*dz*1e3; % 中心点z，单位mm
+side = 25; % 25mm
+
+x1 = center_x_mm - side/2;
+x2 = center_x_mm + side/2;
+z1 = center_z_mm - side/2;
+z2 = center_z_mm + side/2;
 
 figure;
 subplot(1,2,1);
@@ -315,88 +340,164 @@ imagesc(z, x, medium.density);
 xlabel('z [mm]'); ylabel('x [mm]');
 colorbar; title('Density Map');
 axis equal tight;
+hold on;
+rectangle('Position', [z1, x1, side, side], 'EdgeColor', 'r', 'LineWidth', 2);
+hold off;
 
 subplot(1,2,2);
 imagesc(z, x, medium.sound_speed);
 xlabel('z [mm]'); ylabel('x [mm]');
 colorbar; title('Sound Speed Map');
 axis equal tight;
+hold on;
+rectangle('Position', [z1, x1, side, side], 'EdgeColor', 'r', 'LineWidth', 2);
+hold off;
 
 %--------------------------------------------------------------------------%
-% % k-Wave calculation
-% 
-% disp('Launching kWave. This can take a while.');
-% source_strength = 1e6; %Pa
-% source.ux = toneBurst(1/kgrid.dt, f0, cycles,'Envelope','Rectangular');   
-% source.ux = (source_strength ./ (1540*rho0)) .* source.ux;
-% %source.ux = filterTimeSeries(kgrid, medium,source.ux,'PlotSpectrums',true,'ZeroPhase',true); % low-pass 
-% source.uy = 0;
-% source.u_mode ='dirichlet'; %?
-% sensor.record={'p','p_final'};
+% k-Wave calculation
+
+disp('Launching kWave. This can take a while.');
+source_strength = 1e6; %Pa
+source.ux = toneBurst(1/kgrid.dt, f0, cycles,'Envelope','Rectangular');   
+source.ux = (source_strength ./ (1540*rho0)) .* source.ux;
+%source.ux = filterTimeSeries(kgrid, medium,source.ux,'PlotSpectrums',true,'ZeroPhase',true); % low-pass 
+source.uy = 0;
+source.u_mode ='dirichlet'; %?
+sensor.record={'p','p_final'};
 % input_args = {'PMLInside', false, 'PMLSize', [pml_x_size, pml_y_size],'PlotPML', false, 'Smooth', false}; 
-% % input_args = {'PMLInside', false, 'PMLSize', [pml_x_size, pml_y_size],'PlotPML', false, 'Smooth', false,'PlotLayout',true}; 
-% % run the simulation
+input_args = {'PMLInside', false, 'PMLSize', [pml_x_size, pml_y_size],'PlotPML', false, 'Smooth', false,'PlotLayout',true}; 
+% run the simulation
+sensor_data = permute(kspaceFirstOrder2D(kgrid, medium, source, sensor, input_args{:}),[2 1]);
 % sensor_data = permute(kspaceFirstOrder2DG(kgrid, medium, source, sensor, input_args{:}),[2 1]);
-% sensor_data.p(isnan(sensor_data.p))=0;
-% 
-% % assign time series signals to channels accordingly for beamforming 
-% % Gather element signals
-% real_sensor_data=sensor_data.p';
-% %without defining kerf 
-% groupspacing = 11; % 11 if considering kerf 
-% dim_size=size(real_sensor_data);
-% sumnum = groupspacing-1 ; 
-% element=1;
-% element_data=zeros(time_steps+1,256); %128
-% for m = 1:groupspacing:dim_size(2)-sumnum
-%     element_data(:,element) = (real_sensor_data(:,m)+ real_sensor_data(:,m+1)+ real_sensor_data(:,m+2)+ real_sensor_data(:,m+3)+ real_sensor_data(:,m+4)+ real_sensor_data(:,m+5)+ real_sensor_data(:,m+6)+ real_sensor_data(:,m+7)+real_sensor_data(:,m+8)+real_sensor_data(:,m+9)+real_sensor_data(:,m+10));
-%     % without defining kerf
-%     %element_data(:,element) = (real_sensor_data(:,m)+ real_sensor_data(:,m+1)+ real_sensor_data(:,m+2)+ real_sensor_data(:,m+3)+ real_sensor_data(:,m+4)+ real_sensor_data(:,m+5)+ real_sensor_data(:,m+6)+ real_sensor_data(:,m+7)+real_sensor_data(:,m+8)+real_sensor_data(:,m+9)+real_sensor_data(:,m+10)+real_sensor_data(:,m+11)) ;
-%     element = element+1;
-% end 
-% 
-% 
-% % Time gain compensation 
-% % create time gain compensation function based on attenuation value and
-% % round trip distance
-% element_data(1,:)=0;
-% t0 = length(source.ux) * kgrid.dt/2;
-% r = 1540*( (1:length(kgrid.t_array)) * kgrid.dt - t0 ) / 2;   
-% tone_burst_freq=f0;
-% tgc_alpha_db_cm = medium.alpha_coeff * (tone_burst_freq * 1e-6)^medium.alpha_power;
-% tgc_alpha_np_m = tgc_alpha_db_cm / 8.686 * 100;
-% tgc = exp(tgc_alpha_np_m * 2 * r);
-% % for u=1:prb.N_elements
-% %      TCG_element_data(:,u) = bsxfun(@times, tgc, element_data(:,u)')';
-% % end 
-% 
-% % frequency filtering 
-% % remove noise by band-pass filtering
-% filtered_channel_data_BP_wide=tools.band_pass(TCG_element_data,1/kgrid.dt,[3e6 4e6 10e6 11e6]); % 
-% non_filtered_channel_data=TCG_element_data;
-% % downsampling to 20 MHz
-% subsampled_channel_data = resample(non_filtered_channel_data,1,8);
-% subsampled_channel_data_filtered_BP_wide = resample(filtered_channel_data_BP_wide,1,8);
-% % zeroing cross-talk artefacts
-% subsampled_channel_data(1:100,:)=0;
-% subsampled_channel_data_filtered_BP_wide(1:100,:)=0;
-% 
-% % % beamforming with ustb
-% % % create a channel_data handle 
-% % channel_data = uff.channel_data();
-% % channel_data.probe = prb;
-% % channel_data.sequence=seq;
-% % channel_data.initial_time=0;
-% % channel_data.sampling_frequency = 20000000;
-% % channel_data.data = subsampled_channel_data_filtered_BP_wide;
-% % channel_data.data(isnan(channel_data.data))=0;
-% 
-% % save variables for training 
-% sos_map(:,:,i) = medium_sos;
-% sos_map_echo(:,:,i)= medium.sound_speed; 
-% % RF data
-% raw_data_noTCG(:,:,i)=element_data; % no-filter-no-time-gain-no-subsampled
-% wide_filtered_rf(:,:,i)=subsampled_channel_data_filtered_BP_wide; 
-% no_filtered_rf(:,:,i)=subsampled_channel_data;
-% 
+sensor_data.p(isnan(sensor_data.p))=0;
+
+% assign time series signals to channels accordingly for beamforming 
+% Gather element signals
+real_sensor_data=sensor_data.p';
+%without defining kerf 
+groupspacing = 11; % 11 if considering kerf 
+dim_size=size(real_sensor_data);
+sumnum = groupspacing-1 ; 
+element=1;
+element_data=zeros(time_steps+1,256); %128
+for m = 1:groupspacing:dim_size(2)-sumnum
+    element_data(:,element) = (real_sensor_data(:,m)+ real_sensor_data(:,m+1)+ real_sensor_data(:,m+2)+ real_sensor_data(:,m+3)+ real_sensor_data(:,m+4)+ real_sensor_data(:,m+5)+ real_sensor_data(:,m+6)+ real_sensor_data(:,m+7)+real_sensor_data(:,m+8)+real_sensor_data(:,m+9)+real_sensor_data(:,m+10));
+    % without defining kerf
+    %element_data(:,element) = (real_sensor_data(:,m)+ real_sensor_data(:,m+1)+ real_sensor_data(:,m+2)+ real_sensor_data(:,m+3)+ real_sensor_data(:,m+4)+ real_sensor_data(:,m+5)+ real_sensor_data(:,m+6)+ real_sensor_data(:,m+7)+real_sensor_data(:,m+8)+real_sensor_data(:,m+9)+real_sensor_data(:,m+10)+real_sensor_data(:,m+11)) ;
+    element = element+1;
 end 
+
+
+% Time gain compensation 
+% create time gain compensation function based on attenuation value and
+% round trip distance
+element_data(1,:)=0;
+t0 = length(source.ux) * kgrid.dt/2;
+r = 1540*( (1:length(kgrid.t_array)) * kgrid.dt - t0 ) / 2;   
+tone_burst_freq=f0;
+tgc_alpha_db_cm = medium.alpha_coeff * (tone_burst_freq * 1e-6)^medium.alpha_power;
+tgc_alpha_np_m = tgc_alpha_db_cm / 8.686 * 100;
+tgc = exp(tgc_alpha_np_m * 2 * r);
+% for u=1:prb.N_elements
+%      TCG_element_data(:,u) = bsxfun(@times, tgc, element_data(:,u)')';
+% end 
+
+% frequency filtering 
+% remove noise by band-pass filtering
+filtered_channel_data_BP_wide=tools.band_pass(TCG_element_data,1/kgrid.dt,[3e6 4e6 10e6 11e6]); % 
+non_filtered_channel_data=TCG_element_data;
+% downsampling to 20 MHz
+subsampled_channel_data = resample(non_filtered_channel_data,1,8);
+subsampled_channel_data_filtered_BP_wide = resample(filtered_channel_data_BP_wide,1,8);
+% zeroing cross-talk artefacts
+subsampled_channel_data(1:100,:)=0;
+subsampled_channel_data_filtered_BP_wide(1:100,:)=0;
+
+% % beamforming with ustb
+% % create a channel_data handle 
+% channel_data = uff.channel_data();
+% channel_data.probe = prb;
+% channel_data.sequence=seq;
+% channel_data.initial_time=0;
+% channel_data.sampling_frequency = 20000000;
+% channel_data.data = subsampled_channel_data_filtered_BP_wide;
+% channel_data.data(isnan(channel_data.data))=0;
+
+% save variables for training 
+sos_map(:,:,i) = medium_sos;
+sos_map_echo(:,:,i)= medium.sound_speed; 
+% RF data
+raw_data_noTCG(:,:,i)=element_data; % no-filter-no-time-gain-no-subsampled
+wide_filtered_rf(:,:,i)=subsampled_channel_data_filtered_BP_wide; 
+no_filtered_rf(:,:,i)=subsampled_channel_data;
+
+end 
+
+%% channel-wise normalisation/standardalisation; sos pre-processing 
+% RF data
+for j = 1:total_sim
+    for i = 1:128
+       % wide_filtered_rf_normalized(:,i,j)=normalize(wide_filtered_rf(:,i,j));
+        non_filtered_rf_normalized(:,i,j)=normalize(no_filtered_rf(:,i,j));
+    end 
+    sos_map_echo_d2(:,:,j)=imresize(sos_map_echo(:,:,j),[384,384],'bilinear'); 
+    sos_map_d2(:,:,j)=imresize(sos_map(:,:,j),[384,384],'bilinear');
+end 
+
+%% noise addition 
+% quantization noise 
+% quantizing a real signal to 64 bits of prescion can be modelled as a
+% linear system that adds normally distributed noise with a standard
+% deviation of 2^(-p)/sqrt(12) theoretically 
+% zero mean 
+% std_quantized_noise = 2^(-64)/sqrt(12);
+% noise_vals = std_quantized_noise*randn(1024,128)+0;
+% non_filtered_rf_normalized = non_filtered_rf_normalized+noise_vals;
+%
+% add thermal noise to each channel with snr [40 120]dB 
+for i =1:total_sim 
+    snr_val = 40+80*rand(1);
+    no_filtered_rf_norm_noisy(:,:,i)=awgn(non_filtered_rf_normalized(:,:,i),snr_val,'measured');
+end 
+% add realistic noise 
+% index = randi([1 2000],1000,1);
+% no_filtered_rf_norm_noisy(1:100,:,index)=noise_normalized(1:100,:,randi([1 256],1000,1));
+
+% 获取当前日期
+current_date = datestr(now, 'mmdd_HHMMSS');
+% 生成唯一的文件名
+filename = sprintf('data_%s.mat', current_date);
+% 保存所有数据到同一个 .mat 文件
+save(filename, 'non_filtered_rf_normalized', 'sos_map_d2');
+%% check
+   % 生成5张图像
+num_images = 5;
+num_samples = 4;
+total_maps = size(sos_map_d2, 3);
+
+for img_idx = 1:num_images
+    % 随机选择4个索引
+    random_indices = randperm(total_maps, num_samples);
+
+    % 创建一个新的图形窗口
+    figure;
+
+    % 绘制4个子图
+    for i = 1:num_samples
+        subplot(2, 2, i); % 创建2行2列的子图布局
+        imagesc(sos_map_d2(:, :, random_indices(i))); % 绘制声速图
+        colormap gray; % 设置颜色映射为灰度
+        colorbar; % 显示颜色条
+        axis equal tight; % 设置坐标轴比例相等并紧凑显示
+    end
+
+    % 设置整体图像标题
+    sgtitle('Randomly Selected Speed of Sound Maps');
+
+    % 保存图像到文件
+    filename = sprintf('sos_maps_%02d.png', img_idx);
+    saveas(gcf, filename);
+
+    % 关闭当前图像窗口
+    close(gcf);
+end
